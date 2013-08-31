1df68ceb3c97a2e2d07546629c16ebff3af27e33
Nikhil N <nikhiln@outlook.com>
2013-08-31 23:19:38 +0530

    Update Employee.m

--- Lab 2/Employee/Employee.m
+++ Lab 2/Employee/Employee.m
@@ -2,26 +2,30 @@
 
 @implementation Employee
 
--setName:(const char*)name1{
-strcpy(name,name1);
-return 0;
+-setName:(const char*)name1
+{
+  strcpy(name,name1);
+  return 0;
 }
 
--setDept:(const char*)dept1{
-strcpy(dept,dept1);
-return 0;
+-setDept:(const char*)dept1
+{
+  strcpy(dept,dept1);
+  return 0;
 }
 
--setID:(const char*)id1{
-strcpy(ID,id1);
-return 0;
+-setID:(const char*)id1
+{
+  strcpy(ID,id1);
+  return 0;
 }
 
-- print{
-printf("Name : %s \n",name);
-printf("Dept : %s \n",dept);
-printf("ID : %s \n",ID);
-return 0;
+- print
+{
+  printf("Name : %s \n",name);
+  printf("Dept : %s \n",dept);
+  printf("ID : %s \n",ID);
+  return 0;
 }
 
 @end
