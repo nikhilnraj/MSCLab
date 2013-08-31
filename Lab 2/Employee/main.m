782357c58151cd7e82cf6bb7bd101d4dfeb487ff
Nikhil N <nikhiln@outlook.com>
2013-08-31 23:20:15 +0530

    Update main.m

--- Lab 2/Employee/main.m
+++ Lab 2/Employee/main.m
@@ -1,14 +1,15 @@
 #import<Foundation/Foundation.h>
 #import "Employee.h"
 
-int main(int argc, const char * argv[]){
-Employee *emp = [[Employee alloc]init];
+int main(int argc, const char * argv[])
+{
+  Employee *emp = [[Employee alloc]init];
 
-[emp setName:"Nikhil"];
-[emp setDept:"ISE"];
-[emp setID:"1pi10is062"];
+  [emp setName:"Nikhil"];
+  [emp setDept:"ISE"];
+  [emp setID:"1pi10is062"];
 
-[emp print];
-[emp release];
-return 0;
+  [emp print];
+  [emp release];
+  return 0;
 }
